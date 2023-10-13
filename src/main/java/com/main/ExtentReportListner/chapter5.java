8159a58ac3d89b30b62e8fad7c2305dfa3807380
anonymousauthentication <manoharkant.joshi562@webkul.in>
2023-10-13 23:21:17 +0530

    Different attribute to  show in report author , category, device

---
+++ src/main/java/com/main/ExtentReportListner/chapter5.java
@@ -0,0 +1,46 @@
+package com.main.ExtentReportListner;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class chapter5 {
+	public static void main(String args[]) throws IOException {
+		ExtentReports extentReports = new ExtentReports();
+		ExtentSparkReporter sparkReporter = new ExtentSparkReporter("report5.html");
+		extentReports.attachReporter(sparkReporter); 
+		
+		extentReports.createTest("Test 1")
+		.assignAuthor("Manohar")
+		.assignCategory("Smoke")
+		.assignDevice("Chrome 101")
+		.pass("Pass Test");
+		
+		extentReports.createTest("Test 2 ")
+		.assignAuthor("John Doe")
+		.assignCategory("Sanity")
+		.assignDevice("Edge 50")
+		.fail("fail Test");
+		
+		extentReports.createTest("Test 2 ")
+		.assignAuthor("Peter rack")
+		.assignCategory("Regression")
+		.assignDevice("Firefox 102")
+		.fail("fail Test");
+
+		extentReports.createTest("Test 2 ")
+		.assignAuthor("Peter rack")
+		.assignAuthor("Manohar")
+		.assignCategory("Regression")
+		.assignCategory("Smoke")
+		.assignDevice("Firefox 102")
+		.assignDevice("Edge 102")
+		.skip("fail Test");
+		
+		extentReports.flush();
+		Desktop.getDesktop().browse(new File("report5.html").toURI());
+	}
+}
